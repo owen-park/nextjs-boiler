6e6813e1085f98779d5a6370db49148f634d2785
owen-park <parkws18@gmail.com>
2025-05-05 09:36:47 +0900

    init

---
+++ layout/AppFooter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppFooter = () => {
+    return (
+        <div className="layout-footer">
+            by
+            <span className="font-medium ml-2">OWEN</span>
+        </div>
+    );
+};
+
+export default AppFooter;
